26a02baf26975dc5bdefac2a57327fe5ef9f5fc2
Lamduy0807 <19521421@gm.uit.edu.vn>
2020-11-13 10:46:36 +0700

    Change form + add loading screen + greeting screen

---
+++ Form2.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Game_Xếp_Hình
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(375, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(378, 107);
+            this.button1.TabIndex = 12;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.AutoSize = true;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(375, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(378, 107);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1111, 642);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 642);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ Form3.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Game_Xếp_Hình
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.bExit = new System.Windows.Forms.Button();
+            this.bNew = new System.Windows.Forms.Button();
+            this.bPause = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvcapdo = new System.Windows.Forms.Label();
+            this.lbtime = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbdiem = new System.Windows.Forms.Label();
+            this.pbmain = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbmain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bExit
+            // 
+            this.bExit.BackColor = System.Drawing.Color.Crimson;
+            this.bExit.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bExit.Location = new System.Drawing.Point(315, 446);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(100, 55);
+            this.bExit.TabIndex = 21;
+            this.bExit.Text = "Exit";
+            this.bExit.UseVisualStyleBackColor = false;
+            // 
+            // bNew
+            // 
+            this.bNew.BackColor = System.Drawing.Color.Crimson;
+            this.bNew.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bNew.Location = new System.Drawing.Point(187, 444);
+            this.bNew.Name = "bNew";
+            this.bNew.Size = new System.Drawing.Size(100, 55);
+            this.bNew.TabIndex = 20;
+            this.bNew.Text = "Game Mới";
+            this.bNew.UseVisualStyleBackColor = false;
+            // 
+            // bPause
+            // 
+            this.bPause.BackColor = System.Drawing.Color.Crimson;
+            this.bPause.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bPause.Location = new System.Drawing.Point(59, 444);
+            this.bPause.Name = "bPause";
+            this.bPause.Size = new System.Drawing.Size(100, 55);
+            this.bPause.TabIndex = 19;
+            this.bPause.Text = "Tạm Dừng";
+            this.bPause.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(509, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 32);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Thời Gian:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(518, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 32);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Điểm: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lvcapdo
+            // 
+            this.lvcapdo.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvcapdo.ForeColor = System.Drawing.Color.Black;
+            this.lvcapdo.Location = new System.Drawing.Point(846, 9);
+            this.lvcapdo.Name = "lvcapdo";
+            this.lvcapdo.Size = new System.Drawing.Size(80, 32);
+            this.lvcapdo.TabIndex = 15;
+            this.lvcapdo.Text = "cấp";
+            this.lvcapdo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbtime
+            // 
+            this.lbtime.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtime.ForeColor = System.Drawing.Color.Black;
+            this.lbtime.Location = new System.Drawing.Point(628, 56);
+            this.lbtime.Name = "lbtime";
+            this.lbtime.Size = new System.Drawing.Size(89, 32);
+            this.lbtime.TabIndex = 14;
+            this.lbtime.Text = "time";
+            this.lbtime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(739, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Cấp Độ:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbdiem
+            // 
+            this.lbdiem.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdiem.ForeColor = System.Drawing.Color.Black;
+            this.lbdiem.Location = new System.Drawing.Point(593, 9);
+            this.lbdiem.Name = "lbdiem";
+            this.lbdiem.Size = new System.Drawing.Size(89, 32);
+            this.lbdiem.TabIndex = 18;
+            this.lbdiem.Text = "điểm";
+            this.lbdiem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbmain
+            // 
+            this.pbmain.Image = ((System.Drawing.Image)(resources.GetObject("pbmain.Image")));
+            this.pbmain.Location = new System.Drawing.Point(523, 119);
+            this.pbmain.Name = "pbmain";
+            this.pbmain.Size = new System.Drawing.Size(507, 391);
+            this.pbmain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbmain.TabIndex = 3;
+            this.pbmain.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.InitialImage = null;
+            this.pb9.Location = new System.Drawing.Point(318, 271);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(125, 125);
+            this.pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb9.TabIndex = 10;
+            this.pb9.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.InitialImage = null;
+            this.pb6.Location = new System.Drawing.Point(318, 140);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(125, 125);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb6.TabIndex = 9;
+            this.pb6.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.InitialImage = null;
+            this.pb3.Location = new System.Drawing.Point(318, 9);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(125, 125);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 8;
+            this.pb3.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.InitialImage = null;
+            this.pb8.Location = new System.Drawing.Point(187, 271);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(125, 125);
+            this.pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb8.TabIndex = 7;
+            this.pb8.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.InitialImage = null;
+            this.pb7.Location = new System.Drawing.Point(56, 271);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(125, 125);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb7.TabIndex = 6;
+            this.pb7.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.InitialImage = null;
+            this.pb5.Location = new System.Drawing.Point(187, 140);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(125, 125);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb5.TabIndex = 5;
+            this.pb5.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.InitialImage = null;
+            this.pb4.Location = new System.Drawing.Point(56, 140);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(125, 125);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb4.TabIndex = 4;
+            this.pb4.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.InitialImage = null;
+            this.pb2.Location = new System.Drawing.Point(187, 9);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(125, 125);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 11;
+            this.pb2.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.InitialImage = null;
+            this.pb1.Location = new System.Drawing.Point(56, 9);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(125, 125);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 12;
+            this.pb1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 551);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bNew);
+            this.Controls.Add(this.bPause);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvcapdo);
+            this.Controls.Add(this.lbtime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbdiem);
+            this.Controls.Add(this.pbmain);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbmain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bExit;
+        private System.Windows.Forms.Button bNew;
+        private System.Windows.Forms.Button bPause;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lvcapdo;
+        private System.Windows.Forms.Label lbtime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbdiem;
+        private System.Windows.Forms.PictureBox pbmain;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
